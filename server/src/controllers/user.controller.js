24eecb4724d7d6cb46a595d1aad0ac21a3507881
GouranshAgarwal <145385192+GouranshAgarwal@users.noreply.github.com>
2026-03-27 19:14:53 +0530

    added system architecture

--- server/src/controllers/user.controller.js
+++ server/src/controllers/user.controller.js
@@ -1,10 +1,22 @@
 import {asyncHandler} from "../utils/asyncHandler.js"
 
 const registerUser = asyncHandler( async(req, res) => {
-    res.status(200).json({
-        success: true,
-        message: "user registred",
-    });
+    /*
+        destructure req to get data
+        validate data
+        check if already registerd
+        if yes -- redirect to login else continue
+        check for image/videos 
+        validate for required and format
+        upload on cloudinary
+        create user object
+        put object in database 
+        check for response
+        return response -- remove refresh token and password from response
+    */
+   const { username, email, fullName, password } = req.body;
+   console.log("data from req: ", username);
+   
 } )
 
 export { registerUser };
